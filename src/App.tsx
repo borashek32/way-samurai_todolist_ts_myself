98715b9aa5aa369644ad873f3230cf59204a8690
borashek <borashek@inbox.ru>
2022-11-21 09:57:56 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Todolist} from "./components/Todolist";
+
+let tasks: Array<TaskType> = [
+  {id: 1, title: "HTML&CSS", isDone: true, priority: "high"},
+  {id: 2, title: "JS", isDone: true, priority: "low"},
+  {id: 3, title: "ReactJS", isDone: false, priority: "middle"},
+  {id: 4, title: "HTML&CSS1", isDone: true, priority: "middle"},
+  {id: 5, title: "JS1", isDone: true, priority: "high"},
+  {id: 6, title: "ReactJS1", isDone: false, priority: "low"}
+]
+
+export type TaskType = {
+  id: number
+  title: string
+  isDone: boolean
+  priority: "all" | FilterType
+}
+
+export type FilterType = "all" | TaskPriorityType
+
+export type TaskPriorityType = "low" | "middle" | "high"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist name={"Todolist"} tasks={tasks} />
     </div>
   );
 }
